7a13fcbe224327be93f3f58d92dab42aff9971dd
JM <>
2022-10-07 20:23:15 +0300

    Add a chat service

---
+++ chatservice/src/main/java/com/scrieme/chatservice/config/model/Status.java
@@ -0,0 +1,7 @@
+package com.scrieme.chatservice.config.model;
+
+public enum Status {
+    JOIN,
+    MESSAGE,
+    LEAVE
+}
---
+++ chatservice/src/test/java/com/scrieme/chatservice/ChatserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scrieme.chatservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
